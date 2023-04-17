32846d0ddbc99f415adf693fc4291123a2dab59d
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2023-04-18 01:51:28 +0700

    dd

--- thiduaanhson/blog/views.py
+++ thiduaanhson/blog/views.py
@@ -12,13 +12,22 @@ def index(request):
     latest_post = Post.objects.latest('created_time')
     latest_posts = Post.objects.all().exclude(id=latest_post.id).order_by('-created_time')[:4]
     quotes = Quote.objects.all()
+    home_post2 = Post.objects.filter(tags=13).latest('created_time')
+    home_post3 = Post.objects.filter(tags=14).latest('created_time')
+    author_liked = sum(post.like for post in Post.objects.filter(author=home_post2.author))
+    author2_liked = sum(post.like for post in Post.objects.filter(author=home_post3.author))
+
     context = {
         'videos': videos,
         'slides': slides,
         'highlight_post': highlight_post,
         'latest_posts': latest_posts,
         'latest_post': latest_post,
-        'quotes': quotes
+        'quotes': quotes,
+        'home_post2': home_post2,
+        'author_liked': author_liked,
+        'home_post3': home_post3,
+        'author2_liked': author2_liked
     }
 
     return render(request, 'index.html', context)
@@ -71,8 +80,12 @@ def posts(request, tag_id=None):
     if tag_id:
         posts = Post.objects.filter(tags=tag_id).order_by('-created_time')
         tag = get_object_or_404(Tag, pk=tag_id)
+        tag_title = tag.title
+        tag_list = Tag.objects.filter(parrent_tag=tag)
     else:
         posts = Post.objects.all().order_by('-created_time')
+        tag_title = 'Tất cả bài viết'
+        tag_list = Tag.objects.all()
     top_view_posts =  posts.order_by('-view_count')[:5]
     objects = posts
     p = Paginator(objects, 4)
@@ -83,7 +96,9 @@ def posts(request, tag_id=None):
         'list_object': list_object,
         'current_page': current_page,
         'tag_id': tag_id,
-        'tag': tag,
-        'top_view_posts': top_view_posts
+        'tag_title': tag_title,
+        'tag_list': tag_list ,
+        'top_view_posts': top_view_posts,
+        'posts': posts
     }
     return render(request, 'posts.html', context)
